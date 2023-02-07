a17d378886eb8e924952fc4cec975a10c8c49386
Mikey <m.k.blakey@icloud.com>
2023-02-07 11:18:46 +0000

    PCL made for writer2 plus classes made

--- CodeBase/Parser/writewln2.cpp
+++ CodeBase/Parser/writewln2.cpp
@@ -15,9 +15,23 @@
 #include <openbabel/babelconfig.h>
 #include <openbabel/obmolecformat.h>
 
-class WLNSymbol{
-  
+
+const char *wln; 
+
+
+// Carbon decomp can be done at init time 
+
+
+struct WLNSymbol{
+
+  enum type { CARBON = 0, ATOM = 1, FRAGMENT = 2, LINKER = 3}; 
   unsigned char ch; 
+
+  WLNSymbol *dn; // should be a single term
+  WLNSymbol *ac; // linked list of across chains 
+
+
+
 
 
 
@@ -26,10 +40,48 @@ class WLNSymbol{
 
 
 
-int main(){
+static void DisplayUsage(){
+  fprintf(stderr,"wln-writer <input> (escaped)\n");
+  exit(1);
+}
 
 
-  fprintf(stderr,"Hello World\n");
+static unsigned int ProcessCommandLine(int argc, char *argv[]){
+
+  const char *ptr=0; 
+  int i,j; 
+
+  wln = (const char*)0; 
+  
+  if (argc < 2)
+   DisplayUsage();
+
+  j=0; 
+  for (i=1;i<argc;i++){
+
+    ptr = argv[i];
+
+    if (ptr[0]=='-' && ptr[1])
+      fprintf(stderr,"Error: writer only takes in single strings, option detected!\n");
+    
+    else switch(j++){
+      case 0: wln = ptr; break;
+      default: break;
+    }
+      
+    // end argc loop
+  }
+
+}
+
+
+
+int main(int argc, char *argv[]){
+
+
+  ProcessCommandLine(argc, argv);
+
+  fprintf(stderr,"wln: %s\n",wln);
 
   return 0;
 }
