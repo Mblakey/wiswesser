a0563f4f9661b9fa6e416e825084ba93daef226f
Mikey <MBlakey@github.com>
2023-03-29 10:41:32 +0100

    tree restructure

---
+++ src/parser/build/CMakeFiles/wln-writer3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wln-writer3.dir/writewln3.cpp.o"
+  "CMakeFiles/wln-writer3.dir/writewln3.cpp.o.d"
+  "wln-writer3"
+  "wln-writer3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wln-writer3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
