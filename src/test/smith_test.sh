bdb2c0ecd4daebb9751adca37c137d995f9efefd
Mikey <MBlakey@github.com>
2023-05-02 20:18:56 +0100

    found source of bus error - un init pointer

--- src/test/smith_test.sh
+++ src/test/smith_test.sh
@@ -11,10 +11,16 @@ COUNT=0
 TOTAL=$(wc -l < $SMITH)
 
 while read p; do
-	WLN=$(echo "$p" | cut -d $'\t' -f1)
-  SMILES=$(echo "$p" | cut -d $'\t' -f2)
+	WLN=$(echo -n "$p" | cut -d $'\t' -f1)
+  SMILES=$(echo -n "$p" | cut -d $'\t' -f2)
   
   NEW_SMILES=$($PARSE $WLN 2> /dev/null)
+
+  if [ -z $NEW_SMILES ]; then
+    echo "$WLN != $SMILES"
+    continue
+  fi;
+
   NEW_SMILES=${NEW_SMILES::-1}
 
   if [[ "$SMILES" == "$NEW_SMILES" ]]; then
