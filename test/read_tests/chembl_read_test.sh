be5e3c28bfdc11894475edeb982120e82285cad7
Mikey <MBlakey@github.com>
2023-08-10 16:32:32 +0100

    working through the chembl file, some bugs to fix with carbon counts, but progress is good

--- test/read_tests/chembl_read_test.sh
+++ test/read_tests/chembl_read_test.sh
@@ -2,22 +2,23 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-PUB="${SCRIPT_DIR}/../data/pubchem.tsv"
-PARSE="${SCRIPT_DIR}/../src/parser/build/readwln"
-CANONICAL="${SCRIPT_DIR}/../src/parser/build/obabel_strip"
+CHEMBL="${SCRIPT_DIR}/../../data/chembl24.tsv"
+PARSE="${SCRIPT_DIR}/../../src/parser/build/readwln"
+CANONICAL="${SCRIPT_DIR}/../../src/parser/build/obabel_strip"
 
 COUNT=0
-TOTAL=$(wc -l < $PUB)
+TOTAL=$(wc -l < $CHEMBL)
 
 LINE=0
 while read p; do
   ((LINE++));
 
 	WLN=$(echo -n "$p" | cut -d $'\t' -f1)
-  SMILES=$(echo -n "$p" | cut -d $'\t' -f2)
+  SMILES=$(echo -n "$p" | cut -d $'\t' -f3)
   
 
   CAN_SMILES=$($CANONICAL "$SMILES" 2> /dev/null)
+
   NEW_SMILES=$($PARSE -ocan -s "${WLN}" 2> /dev/null) # chembl is canonical smiles
 
   if [ -z $NEW_SMILES ]; then
@@ -34,7 +35,7 @@ while read p; do
     echo "$LINE: $WLN != $CAN_SMILES    $NEW_SMILES"
   fi;
 
-done <$PUB
+done <$CHEMBL
 
 echo -ne "\r$COUNT/$TOTAL correct\n"
 echo "unit test complete"
--- test/read_tests/chemspider_read_test.sh
+++ test/read_tests/chemspider_read_test.sh
@@ -2,40 +2,37 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-CHEMBL="${SCRIPT_DIR}/../data/chembl24.tsv"
-PARSE="${SCRIPT_DIR}/../src/parser/build/readwln"
-CANONICAL="${SCRIPT_DIR}/../src/parser/build/obabel_strip"
+SPIDER="${SCRIPT_DIR}/../../data/chemspider.tsv "
+PARSE="${SCRIPT_DIR}/../../src/parser/build/readwln"
+CANONICAL="${SCRIPT_DIR}/../../src/parser/build/obabel_strip"
 
 COUNT=0
-TOTAL=$(wc -l < $CHEMBL)
-
+TOTAL=$(wc -l < $SPIDER)
 LINE=0
 while read p; do
   ((LINE++));
-
 	WLN=$(echo -n "$p" | cut -d $'\t' -f1)
-  SMILES=$(echo -n "$p" | cut -d $'\t' -f3)
+  SMILES=$(echo -n "$p" | cut -d $'\t' -f2)
   
-
+  NEW_SMILES=$($PARSE -c -ocan -s "${WLN}" 2> /dev/null)
   CAN_SMILES=$($CANONICAL "$SMILES" 2> /dev/null)
 
-  NEW_SMILES=$($PARSE -ocan -s "${WLN}" 2> /dev/null) # chembl is canonical smiles
-
   if [ -z $NEW_SMILES ]; then
-    echo "$LINE: $WLN != anything"
+    echo "$LINE: $WLN != $CAN_SMILES"
     continue
   fi;
 
+  #NEW_SMILES=${NEW_SMILES::-1}
   NEW_SMILES=${NEW_SMILES:0:${#NEW_SMILES}-1}
   CAN_SMILES=${CAN_SMILES:0:${#CAN_SMILES}-1}
 
   if [[ "$CAN_SMILES" == "$NEW_SMILES" ]]; then
   	((COUNT++));
   else
-    echo "$LINE: $WLN != $CAN_SMILES    $NEW_SMILES"
+  	echo "$LINE: $WLN != $CAN_SMILES    $NEW_SMILES"
   fi;
 
-done <$CHEMBL
+done <$SPIDER
 
 echo -ne "\r$COUNT/$TOTAL correct\n"
 echo "unit test complete"
