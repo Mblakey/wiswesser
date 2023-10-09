efd4aed1b80aa2db7ed58b57abd2354345620a27
Mikey <MBlakey@github.com>
2023-10-09 13:47:01 +0100

    blossums for wln rings added

--- src/parser/readwln2.cpp
+++ src/parser/readwln2.cpp
@@ -161,7 +161,6 @@ struct WLNRing
 
   unsigned int rsize;
   unsigned int aromatic_atoms;
-
   unsigned int *adj_matrix; 
 
   std::map<unsigned char, WLNSymbol *>      locants; 
@@ -218,6 +217,177 @@ struct WLNRing
   }
 
 };
+
+struct WLNBlossom{
+  int n; 
+  int m; 
+
+  std::vector<int> mate; 
+  std::vector<int> p,d,bl;
+  std::vector<std::vector<int>> b,g; 
+
+  WLNBlossom(int _n){
+    n = _n;
+    m = n+n / 2;
+    mate.assign(n,-1); 
+
+    b.resize(m);
+    p.resize(m);
+    d.resize(m);
+    bl.resize(m);
+    g.assign(m,std::vector<int>(m,-1));
+  }
+
+  void add_edge(int u, int v){
+    g[u][v] = u;
+    g[v][u] = v;
+  }
+
+  void match(int u, int v){
+    g[u][v] = -1; 
+    g[v][u] = -1;
+    mate[u] = v;
+    mate[v] = u;
+  }
+
+  std::vector<int> trace(int x){
+    std::vector<int> vx; 
+    for(;;){
+      while(bl[x] != x)
+        x = bl[x];
+      
+      if(!vx.empty() && vx.back() == x)
+        break;
+      
+      vx.push_back(x);
+      x = p[x];
+    }
+
+    return vx;
+  }
+
+  void contract(int c, int x, int y, std::vector<int> &vx, std::vector<int> &vy){
+    b[c].clear();
+    int r = vx.back();
+    while(!vx.empty() && !vy.empty() && vx.back() == vy.back()){
+      r = vx.back();
+      vx.pop_back();
+      vy.pop_back();
+    }
+
+    b[c].push_back(r);
+    b[c].insert(b[c].end(),vx.rbegin(),vx.rend());
+    b[c].insert(b[c].end(),vy.rbegin(),vy.rend());
+
+    for(int i = 0; i <= c; i++) 
+      g[c][i] = g[i][c] = -1;
+      
+    for(int z : b[c]) {
+      bl[z] = c;
+      for(int i = 0; i < c; i++) {
+        if(g[z][i] != -1) {
+          g[c][i] = z;
+          g[i][c] = g[i][z];
+        }
+      }
+    }
+  }
+
+  std::vector<int> lift(std::vector<int> &vx) {
+    std::vector<int> A;
+    while(vx.size() >= 2) {
+      int z = vx.back(); 
+      vx.pop_back();
+      if(z < n) {
+        A.push_back(z);
+        continue;
+      }
+      int w = vx.back();
+      int i = (A.size() % 2 == 0 ? std::find(b[z].begin(), b[z].end(), g[z][w]) - b[z].begin() : 0);
+      int j = (A.size() % 2 == 1 ? std::find(b[z].begin(), b[z].end(), g[z][A.back()]) - b[z].begin() : 0);
+      int k = b[z].size();
+      int dif = (A.size() % 2 == 0 ? i % 2 == 1 : j % 2 == 0) ? 1 : k - 1;
+      while(i != j) {
+        vx.push_back(b[z][i]);
+        i = (i + dif) % k;
+      }
+      vx.push_back(b[z][i]);
+    }
+    return A;
+  }
+
+  int solve() {
+    for(int ans = 0; ; ans++) {
+      fill(d.begin(), d.end(), 0);
+      std::queue<int> queue;
+
+      for(int i = 0; i < m; i++) 
+        bl[i] = i;
+
+      for(int i = 0; i < n; i++) {
+        if(mate[i] == -1) {
+          queue.push(i);
+          p[i] = i;
+          d[i] = 1;
+        }
+      }
+
+      int c = n;
+      bool aug = false;
+      while(!queue.empty() && !aug) {
+        int x = queue.front(); 
+        queue.pop();
+          
+        if(bl[x] != x) 
+          continue;
+
+        for(int y = 0; y < c; y++) {
+          
+          if(bl[y] == y && g[x][y] != -1) {
+           
+            if(d[y] == 0){
+              p[y] = x;
+              d[y] = 2;
+              p[mate[y]] = y;
+              d[mate[y]] = 1;
+              queue.push(mate[y]);
+            }
+            else if(d[y] == 1){
+              std::vector<int> vx = trace(x);
+              std::vector<int> vy = trace(y);
+              if(vx.back() == vy.back()) {
+                contract(c, x, y, vx, vy);
+                queue.push(c);
+                p[c] = p[b[c][0]];
+                d[c] = 1;
+                c++;
+              }
+              else {
+                aug = true;
+                vx.insert(vx.begin(), y);
+                vy.insert(vy.begin(), x);
+                std::vector<int> A = lift(vx);
+                std::vector<int> B = lift(vy);
+                A.insert(A.end(), B.rbegin(), B.rend());
+                for(int i = 0; i < (int) A.size(); i += 2) {
+                  match(A[i], A[i + 1]);
+                  if(i + 2 < (int) A.size()) 
+                    add_edge(A[i + 1], A[i + 2]);
+                }
+              }
+              break;
+            }
+          }
+        }
+      }
+
+      if(!aug) 
+        return ans;
+    }
+  }
+
+};
+
 
 
 // handles all memory and 'global' vars
@@ -1820,7 +1990,9 @@ unsigned int BuildCyclic( std::vector<std::pair<unsigned int,unsigned char>> &ri
         }
         edge->aromatic = true;
       }
-         
+        
+
+      // should ring junctions ever be kekulised? 
     }
 
     fuses++;
@@ -3257,42 +3429,17 @@ bool BPMatching(WLNRing *ring, unsigned int u, bool *seen, int *MatchR){
   return false; 
 }
 
-bool WLNRingBPMaxMatching(WLNRing *ring){
+bool WLNRingBPMaxMatching(WLNRing *ring, int *MatchR){
   
   bool  *seen = (bool*)malloc(sizeof(bool) * ring->rsize);
-  int   *MatchR = (int*)malloc(sizeof(int) * ring->rsize);
 
-  for (unsigned int i=0;i<ring->rsize;i++){
+  for (unsigned int i=0;i<ring->rsize;i++)
     seen[i] = false;
-    MatchR[i] = -1;
-  }
-    
-  unsigned int max_matches = 0; 
-
-  for(unsigned int u=0; u< ring->rsize;u++){
-    if(BPMatching(ring,u,seen,MatchR))
-      max_matches++;
-  }
-
-  for(unsigned int i = 0; i<ring->rsize;i++){
-    if(MatchR[i]){
-      WLNSymbol *f = ring->locants[int_to_locant(i+1)];
-      WLNSymbol *s = ring->locants[int_to_locant(MatchR[i]+1)];
-      if(f && s){
-        WLNEdge *edge = search_edge(f,s);
-        edge = unsaturate_edge(edge,1);
-        if(!edge){
-          fprintf(stderr,"Error: failed to unsaturate bond in kekulize\n");
-          return false;
-        }
-
-        MatchR[MatchR[i]] = 0; // remove from matching
-      }
-    }
-  }
-
+  
+  for(unsigned int u=0; u< ring->rsize;u++)
+    BPMatching(ring,u,seen,MatchR);
+      
   free(seen);
-  free(MatchR);
   return true;
 }
 
@@ -3309,9 +3456,51 @@ bool WLNKekulize(WLNGraph &graph){
         return false;
       }
 
-      if(IsBipartite(wring) && !WLNRingBPMaxMatching(wring))
+      int   *MatchR = (int*)malloc(sizeof(int) * wring->rsize);
+      for (unsigned int i=0;i<wring->rsize;i++)
+        MatchR[i] = -1;
+  
+
+      if(IsBipartite(wring) && !WLNRingBPMaxMatching(wring,MatchR))
         return false;
+      else{
+        WLNBlossom B(wring->rsize);
+
+        for(unsigned int u = 0; u < wring->rsize; u++){
+          for(unsigned int v = 0; v < wring->rsize; v++){
+            if(wring->adj_matrix[u * wring->rsize + v] > 0)
+              B.add_edge(u,v);
+          }
+        }
+
+        if(!B.solve())
+          return false;
+
+        for(int i = 0; i < wring->rsize; i++) {
+          if(i < B.mate[i]) {
+            MatchR[i] = B.mate[i];
+            MatchR[B.mate[i]] = i;
+          }
+        }
+      }
+
+      for(unsigned int i = 0; i<wring->rsize;i++){
+        if(MatchR[i] > 0){
+          WLNSymbol *f = wring->locants[int_to_locant(i+1)];
+          WLNSymbol *s = wring->locants[int_to_locant(MatchR[i]+1)];
+          if(f && s){
+            WLNEdge *edge = search_edge(f,s);
+            edge = unsaturate_edge(edge,1);
+            if(!edge){
+              fprintf(stderr,"Error: failed to unsaturate bond in kekulize\n");
+              return false;
+            }
+            MatchR[MatchR[i]] = 0; // remove from matching
+          }
+        }
+      }
       
+      free(MatchR);
     }
   }
 
@@ -4824,13 +5013,20 @@ void WLNDumpToDot(FILE *fp, WLNGraph &graph)
         for (unsigned int k=0;k<bond_order;k++){
           fprintf(fp, "  %d", node->id);
           fprintf(fp, " -> ");
-          fprintf(fp, "%d\n", node->id);
+
+          if(edge->aromatic)
+            fprintf(fp, "%d [color=red]\n", child->id);
+          else
+            fprintf(fp, "%d\n", child->id);
         }
       }
       else{
         fprintf(fp, "  %d", node->id);
         fprintf(fp, " -> ");
-        fprintf(fp, "%d\n", child->id);
+        if(edge->aromatic)
+            fprintf(fp, "%d [color=red]\n", child->id);
+          else
+            fprintf(fp, "%d\n", child->id);
       }
     }
   }
@@ -5156,7 +5352,6 @@ bool ReadWLN(const char *ptr, OBMol* mol)
   if(!ParseWLNString(ptr,wln_graph))
     return false;
 
-  // create an optional wln dotfile
   if (opt_wln2dot)
     WriteGraph(wln_graph,"wln-graph.dot");
   
@@ -5165,6 +5360,10 @@ bool ReadWLN(const char *ptr, OBMol* mol)
 
   if(!WLNKekulize(wln_graph))
     return false; 
+
+  // create an optional wln dotfile
+  // if (opt_wln2dot)
+  //   WriteGraph(wln_graph,"wln-graph.dot");
 
   if(!obabel.ConvertFromWLN(mol,wln_graph))
     return false;
