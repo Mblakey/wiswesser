df4233fedfa67134a9ddccb19f0f1cb7bdbe031b
Mikey <m.k.blakey@icloud.com>
2023-11-11 09:30:01 +0000

    added write to file options for failures

--- test/writing.sh
+++ test/writing.sh
@@ -2,27 +2,35 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+INCORRECT="${SCRIPT_DIR}/incorrect.txt"
 WRITER="${SCRIPT_DIR}/../bin/writewln"
 READER="${SCRIPT_DIR}/../bin/readwln"
 COMP="${SCRIPT_DIR}/../bin/obcomp"
 MODE=""
 FILE=""
 
+WRONG=0
 process_arguments() {
   # Loop through all the arguments
   for arg in "$@"; do
     case "$arg" in
       -h|--help)
         # Display a help message
-        echo "Usage: pubchem_score_test.sh <MODE>"
+        echo "Usage: writing.sh <options> <mode>"
         echo "Modes:"
         echo "  chemspider"
         echo "  pubchem"
         echo "  smith"
         echo "  chembl"
-        echo "Options:"
+        echo "options:"
         echo "  -h, --help          Show this help message"
+        echo "  -w, --wrong         Write failed smiles to incorrect.txt"
         exit 0;
+        ;;
+      
+      -w|--wrong)
+        echo "Failed Smiles" > $INCORRECT
+        WRONG=1
         ;;
       *)
         MODE=$arg
@@ -66,6 +74,11 @@ main(){
 
     if [ -z "$NEW_WLN" ]; then
       echo -ne "$WLN != any new WLN string\t${SMILES}\n$"
+
+      if [ $WRONG -eq 1 ]; then
+        echo "${SMILES}" >> $INCORRECT
+      fi;
+
       continue
     fi;
 
@@ -74,6 +87,11 @@ main(){
     
     if [ -z "$NEW_SMILES" ]; then
       echo "$NEW_WLN != anything $SMILES"
+
+      if [ $WRONG -eq 1 ]; then
+        echo "${SMILES}" >> $INCORRECT
+      fi;
+
       continue
     fi;
 
@@ -83,6 +101,9 @@ main(){
       echo -ne "\r"
     else
       echo -ne  "$WLN == $SMILES\t$NEW_WLN\t$NEW_SMILES\n"
+      if [ $WRONG -eq 1 ]; then
+        echo "${SMILES}" >> $INCORRECT
+      fi;
     fi;
 
   done <$FILE
