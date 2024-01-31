a22d9e6db9d6a50395e6342b63a68c3452e3322b
Mikey <m.k.blakey@icloud.com>
2024-01-31 17:09:35 +0000

    removing descriptor.h - obabel takes care of this

--- src/wlngen/wlngen.cpp
+++ src/wlngen/wlngen.cpp
@@ -6,7 +6,8 @@
 
 #include <iostream>
 #include <string>
-#include <map>
+#include <set>
+#include <unordered_map> // traditional hash map
 #include <vector>
 #include <random>
 
@@ -26,20 +27,22 @@
 #include "wlnmatch.h"
 #include "wlndfa.h"
 #include "parser.h"
-#include "descriptor.h"
-
-unsigned int gen_length = 5;
-unsigned int gen_count = 10; 
-unsigned int opt_verbose = false;
-
-const char *trainfile;
-
 
 using namespace OpenBabel;
 
+int length = 5;
+int count = 10; 
 
-bool train_on_file(FILE *ifp, FSMAutomata *wlnmodel){
-  
+
+int dt = 0;
+double molwt = 0.0;
+double logp = 0.0;
+
+double epsilon = 0.5;
+
+std::vector<const char*> train_files;
+
+bool seed_from_file(FILE *ifp, FSMAutomata *wlnmodel){
   unsigned char ch = 0;
   FSMState *curr = wlnmodel->root;
   FSMEdge *edge = 0;
@@ -70,20 +73,48 @@ bool train_on_file(FILE *ifp, FSMAutomata *wlnmodel){
 }
 
 
-bool Validate(const char * wln_str, OBMol *mol){
+/* 
+--- notes ---
+
+Epsilon-greedy policy - either take an action based on the qtable
+values, (exploitation), or the random FSM values, (exploration).
+
+dummy scoring:
+
++1 = valid WLN
++2 = unique
++5 = in target range
+
+From a good Q table, rescale the probabilities, some might be zero which
+means transitions are removed? Generate compounds
+*/
+
+
+bool Validate(const char *wln_str, OBMol *mol){
   if(!ReadWLN(wln_str,mol))
     return false;
   return true;
 }
 
-double RewardFunction(const char *wln_strm, OBMol *mol){
+
+// https://open-babel.readthedocs.io/en/latest/Descriptors/descriptors.html
+
+
+double LogP(const char *wln_strm, OBMol *mol){
   OBDescriptor* pDesc = OBDescriptor::FindType("logP");
   if(pDesc)
     return pDesc->Predict(mol); 
   else
     return 0.0;
-};
+}
 
+double MolWt(const char *wln_strm, OBMol *mol){
+  OBDescriptor* pDesc = OBDescriptor::FindType("MW");
+  if(pDesc)
+    return pDesc->Predict(mol); 
+  else
+    return 0.0;
+}
 
 FSMEdge *EpsilonGreedy(FSMState *curr, double epsilon, std::mt19937 &rgen){
 
@@ -116,53 +147,14 @@ FSMEdge *EpsilonGreedy(FSMState *curr, double epsilon, std::mt19937 &rgen){
   }
 }
 
-
-/* 
---- notes ---
-
-Epsilon-greedy policy - either take an action based on the qtable
-values, (exploitation), or the random FSM values, (exploration).
-
-I think really the Q table needs to be on the edges per state, since
-two transitions could go to the same state, but have wildly different
-outcomes to the final string. 
-
-logic might follow: mealey Q-learning machine? hmmm
-
-dummy scoring:
-
-+1 = valid WLN
-+2 = unique
-+5 = in target range
-
-From a good Q table, rescale the probabilities, some might be zero which
-means transitions are removed? Generate compounds
-
-
-Mode collapse - not a black box model so there will be methods to handle 
-                this
-*/
-
-
-/* watch for overflows on the unsigned int, floats MAY be better 
-for incremental rewards that level off */
-void debug_Qtable(FSMAutomata *wlnmodel, std::map<FSMEdge*,unsigned int> &Qtable){
-  for(unsigned int i=0;i<wlnmodel->num_edges;i++){
-    FSMEdge *e = wlnmodel->edges[i]; 
-    fprintf(stderr,"%d - %d\n",e->id,Qtable[e]);
-  }
-}
-
 /* uses Q learning to generate compounds from the language FSM
 as a markov decision process, WLN is small enough that with 20
 characters a large scope of chemical space can be covered. */
-bool QGenerateWLN(FSMAutomata *wlnmodel, unsigned int n, unsigned int l){
-  unsigned int hits = 0; 
-  unsigned int misses = 0;
+bool QGenerateWLN(FSMAutomata *wlnmodel){
+  int hits = 0; 
+  int misses = 0;
+  int out_range = 0;
  
-
-  double epsilon = 0.2;
-
   // from the compression data, we can use edge->c to be the q-scoring
 
   std::random_device rd;
@@ -170,31 +162,72 @@ bool QGenerateWLN(FSMAutomata *wlnmodel, unsigned int n, unsigned int l){
 
   FSMState *state = wlnmodel->root; 
   FSMEdge *edge = 0;
-  std::string wlnstr; 
-  std::vector<FSMEdge*> path;
 
-  unsigned int length = 0;
-  while(hits < n){
+  std::string wlnstr; 
+  std::set<FSMEdge*> path; // avoid the duplicate path increases
+  std::unordered_map<std::string, bool> unique;
+
+  int strlength = 0;
+  while(hits < count){
 
     edge = EpsilonGreedy(state,epsilon,gen);
 
     if(edge->ch == '\n'){
 
-      if(length >= l){ // only accepts will have new line, ensures proper molecule
-        length = 0;
+      if(strlength >= length){ // only accepts will have new line, ensures proper molecule
+        strlength = 0;
         state = wlnmodel->root;
 
         // in beta, the faster i make ReadWLN the better this is
         
         OBMol mol;
+        unsigned int score = 0;
         if(Validate(wlnstr.c_str(),&mol)){
-          hits++;
-          fprintf(stdout,"%s = %f\n",wlnstr.c_str(),RewardFunction(wlnstr.c_str(),&mol));
+          score+= 1;
+          
+          if(!unique[wlnstr]){
+            hits++;
+            score += 1;
+            unique[wlnstr] = true;
+
+            double lp = 0.0;
+            double mw = 0.0;
+            switch(dt){
+              case 0:
+                hits++;
+                break;
+
+              case 1:
+                lp = LogP(wlnstr.c_str(),&mol);
+                if (lp >= logp-0.5 && lp <= logp+0.5){
+                  score+= 3;
+                  hits++;
+                  fprintf(stderr,"%s - %f\n",wlnstr.c_str(),lp);
+                }
+                else
+                  out_range++;
+                break;
+
+              case 2: 
+                mw = MolWt(wlnstr.c_str(),&mol);
+                if (mw >= molwt-50 && mw <= molwt+50){
+                  score+= 3;
+                  hits++;
+                  fprintf(stderr,"%s - %f\n",wlnstr.c_str(),mw);
+                }
+                else
+                  out_range++;
+                break;
+            }
+          }
         
           // go back through all the edges and give them the +1 score
-          for(FSMEdge *pe:path){
-            if(pe->c < UINT32_MAX)
-              pe->c++;
+          if(score){
+            path.insert(edge);
+            for(FSMEdge *pe:path){
+              if(pe->c < UINT32_MAX)
+                pe->c+=score;
+            }
           }
         }
         else
@@ -207,12 +240,14 @@ bool QGenerateWLN(FSMAutomata *wlnmodel, unsigned int n, unsigned int l){
         // choose something else
         while(edge->ch == '\n')
           edge = EpsilonGreedy(state,epsilon,gen);
+
+        path.insert(edge);
       }
     }
     else{
-      path.push_back(edge);
+      path.insert(edge);
       wlnstr += edge->ch;
-      length++;
+      strlength++;
     }
    
 
@@ -220,18 +255,29 @@ bool QGenerateWLN(FSMAutomata *wlnmodel, unsigned int n, unsigned int l){
   }
 
 
-  fprintf(stderr,"%d hits, %d misses\n",hits,misses);
+  fprintf(stderr,"%d hits, %d misses, %d out of target range\n",hits,misses,out_range);
   return true;
 }
 
+
+bool prefix(const char *pre, const char *str)
+{
+  return strncmp(pre, str, strlen(pre)) == 0;
+}
 
 static void DisplayUsage()
 {
   fprintf(stderr, "wlngen <options> <trainfile>\n");
   fprintf(stderr,"options:\n");
-  fprintf(stderr,"-l <int>    set length for generation (default 5)\n");
-  fprintf(stderr,"-n <int>    set target count for generation (default 10)\n");
-  fprintf(stderr,"-v          timing and debugging statements to console\n");
+  fprintf(stderr,"-l|--length=<int>      set length for generation (default 5)\n");
+  fprintf(stderr,"-c|--count=<int>       set target count for generation (default 10)\n");
+  fprintf(stderr,"-e|--epsilon=<double>  set epsilon hyperparameter for QL-process (default 0.5)\n");
+  fprintf(stderr,"-p|--print             print all set hyperparameters to console (debugging)\n");
+
+
+  fprintf(stderr,"\ndescriptors:\n");
+  fprintf(stderr,"--logp=<double>     set logp  target value, range is +/- 0.5 from this value\n");
+  fprintf(stderr,"--molwt=<double>    set molwt target value, range is +/- 50  from this value\n");
   exit(1);
 }
 
@@ -239,42 +285,168 @@ static void DisplayUsage()
 static void ProcessCommandLine(int argc, char *argv[])
 {
   const char *ptr = 0;
+  unsigned char ch = 0;
   int i,j;
-
-  trainfile = (const char *)0;
-
   j = 0;
   for (i = 1; i < argc; i++)
   {
     ptr = argv[i];
+    ch = *ptr; 
     if (ptr[0] == '-' && ptr[1]){
       switch (ptr[1]){
-        case 'n':
-          if(i > argc-1){
-            fprintf(stderr,"Error: count flag must be followed by a number\n");
+
+        case 'p':
+          fprintf(stderr,"logp target:       %f\n",logp);
+          fprintf(stderr,"mol weight target: %f\n",molwt);
+          fprintf(stderr,"epsilon value:     %f\n",epsilon);
+          fprintf(stderr,"target count:      %d\n",count);
+          fprintf(stderr,"target legnth:     %d\n",length);
+          fprintf(stderr,"dt mode:           %d\n",dt);
+          exit(0);
+
+        case 'c':
+          while(ch != '=' && ch)
+            ch = *(++ptr);
+
+          if(!(*ptr)){
+            fprintf(stderr,"Error: format for count is -c=<int>\n");
             DisplayUsage();
           }
-          else if(!sscanf(argv[i+1],"%d",&gen_count)){
-            fprintf(stderr,"Error: count flag must be followed by a number\n");
-            DisplayUsage();
+          else{
+            ptr++;
+            count = atoi(ptr);
+            if(count < 0){
+              fprintf(stderr,"Error: count must be a positive integer\n");
+              DisplayUsage();
+            }
           }
-          i++;
           break;
 
         case 'l':
-          if(i > argc-1){
-            fprintf(stderr,"Error: length flag must be followed by a number\n");
+          while(ch != '=' && ch)
+            ch = *(++ptr);
+
+          if(!(*ptr)){
+            fprintf(stderr,"Error: format for legnth is -l=<int>\n");
             DisplayUsage();
           }
-          else if(!sscanf(argv[i+1],"%d",&gen_length)){
-            fprintf(stderr,"Error: length flag must be followed by a number\n");
-            DisplayUsage();
+          else{
+            ptr++;
+            length = atoi(ptr);
+            if(length < 0){
+              fprintf(stderr,"Error: length must be a positive integer\n");
+              DisplayUsage();
+            }
           }
-          i++;
           break;
-        
-        case 'v':
-          opt_verbose = true;
+
+        case 'e':
+          while(ch != '=' && ch)
+            ch = *(++ptr);
+
+          if(!(*ptr)){
+            fprintf(stderr,"Error: format for epsilon is -e=<double>\n");
+            DisplayUsage();
+          }
+          else{
+            ptr++;
+            char* endptr; 
+            epsilon = strtod(ptr, &endptr); 
+            if (epsilon < 0 || epsilon > 1){
+              fprintf(stderr,"Error: range for epsilon is [0,1]\n");
+              DisplayUsage();
+            }
+          }
+          break;
+
+        case '-':
+          ptr++;
+          if(prefix("-logp",ptr)){
+            if(dt){
+              fprintf(stderr,"Error: targeting two descriptors is currently unsupported\n");
+              DisplayUsage();
+            }
+
+            ch = *ptr;
+            while(ch != '=' && ch)
+              ch = *(++ptr); 
+
+            ptr++;
+            char* endptr; 
+            logp = strtod(ptr, &endptr); 
+            dt = 1;
+          }
+          else if(prefix("-molwt",ptr)){
+            if(dt){
+              fprintf(stderr,"Error: targeting two descriptors is currently unsupported\n");
+              DisplayUsage();
+            }
+
+            ch = *ptr;
+            while(ch != '=' && ch)
+              ch = *(++ptr); 
+
+            ptr++;
+            char* endptr; 
+            molwt = strtod(ptr, &endptr); 
+            dt = 2;
+          }
+          else if (prefix("-epsilon",ptr)){
+            while(ch != '=' && ch)
+              ch = *(++ptr);
+            
+            if(!(*ptr)){
+              fprintf(stderr,"Error: format for epsilon is -e=<double>\n");
+              DisplayUsage();
+            }
+            else{
+              ptr++;
+              char* endptr; 
+              epsilon = strtod(ptr, &endptr); 
+              if (epsilon < 0 || epsilon > 1){
+                fprintf(stderr,"Error: range for epsilon is [0,1]\n");
+                DisplayUsage();
+              }
+            }
+          }
+          else if (prefix("-count",ptr)){
+            while(ch != '=' && ch)
+              ch = *(++ptr);
+
+            if(!(*ptr)){
+              fprintf(stderr,"Error: format for count is -c=<int>\n");
+              DisplayUsage();
+            }
+            else{
+              ptr++;
+              count = atoi(ptr);
+              if(count < 0){
+                fprintf(stderr,"Error: count must be a positive integer\n");
+                DisplayUsage();
+              }
+            }
+          }
+          else if (prefix("-length",ptr)){
+            while(ch != '=' && ch)
+              ch = *(++ptr);
+
+            if(!(*ptr)){
+              fprintf(stderr,"Error: format for legnth is -l=<int>\n");
+              DisplayUsage();
+            }
+            else{
+              ptr++;
+              length = atoi(ptr);
+              if(length < 0){
+                fprintf(stderr,"Error: length must be a positive integer\n");
+                DisplayUsage();
+              }
+            }
+          }
+          else{
+            fprintf(stderr,"Error: incorrect input -%s\n",ptr);
+            DisplayUsage();
+          }
           break;
 
         default:
@@ -284,13 +456,8 @@ static void ProcessCommandLine(int argc, char *argv[])
     }
     else{
       switch(j++){
-        case 0:
-          trainfile = ptr; 
-          break;
-
         default:
-          fprintf(stderr,"Error: multiple files not currently supported\n");
-          exit(1);
+          train_files.push_back(ptr); 
       }
     }
   }
@@ -313,18 +480,24 @@ int main(int argc, char *argv[])
 
   wlnmodel->AssignEqualProbs(); // default and prevents div 0
 
-  if(trainfile){
-    FILE *tfp = fopen(trainfile,"r");
-    if(!tfp){
-      fprintf(stderr,"Error: could not open train file\n");
-      return 1;
+  unsigned int f = 0;
+  if(!train_files.empty()){
+    for (const char *trainfile : train_files){
+      f++;
+      FILE *tfp = fopen(trainfile,"r");
+      if(!tfp){
+        fprintf(stderr,"Error: could not open train file %d - skipping\n",f);
+        continue;
+      }
+      else{
+        seed_from_file(tfp,wlnmodel);
+        fclose(tfp);
+        tfp = 0;
+      }
     }
-
-    train_on_file(tfp,wlnmodel);
-    fclose(tfp);
   }
 
-  QGenerateWLN(wlnmodel, gen_count, gen_length);
+  QGenerateWLN(wlnmodel);
   delete wlnmodel;
   return 0;
 }
